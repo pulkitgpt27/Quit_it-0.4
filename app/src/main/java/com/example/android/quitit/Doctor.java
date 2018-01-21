af53d5fab8a2f909b0d34acf213d52e07efabeff
Ayush Vaid <vaid.ayush@gmail.com>
2018-01-21 22:56:50 +0530

    Doctor added. Patient of doctor now shown. Search fixed completely.

--- app/src/main/java/com/example/android/quitit/Doctor.java
+++ app/src/main/java/com/example/android/quitit/Doctor.java
@@ -1,7 +1,6 @@
 package com.example.android.quitit;
 
 import java.util.HashMap;
-import java.util.List;
 /**
  * Created by pulkit gupta on 20/01/2018.
  */
@@ -14,36 +13,39 @@ public class Doctor {
     private HashMap<String,Entry> patients;
     private String degree;
 
+    //*********Constructors***********
     public Doctor(){
     }
+    public Doctor(String mailId){
+        this.mail_id=mailId;
+        this.name = new HashMap<String,String>();
+        this.date_of_birth = new HashMap<String,Integer>();
+        this.patients = new HashMap<String,Entry>();
+    }
 
+    //**********Getters*************
     public String getMail_id() {
         return mail_id;
     }
-
-
     public HashMap<String,String> getName() {
         return name;
     }
-
     public HashMap<String,Integer> getDate_of_birth() {
         return date_of_birth;
     }
-
     public long getPhone_no() {
         return phone_no;
     }
-
     public HashMap<String,Entry> getPatients() {
         return patients;
     }
-
     public String getDegree() {
         return degree;
     }
 
-    public Doctor(String name, String mailId){
-//        this.name=name;
-//        this.mailId=mailId;
+
+    //***********Setters*************
+    public void setMail_id(String mail_id) {
+        this.mail_id = mail_id;
     }
 }
