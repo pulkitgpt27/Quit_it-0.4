82babd2022f550df45bce7993d45b0f7fc5c4294
Ayush Vaid <vaid.ayush@gmail.com>
2018-04-16 13:25:10 +0530

    login changes

--- app/src/main/java/com/example/android/quitit/LoginActivity.java
+++ app/src/main/java/com/example/android/quitit/LoginActivity.java
@@ -25,10 +25,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Pulkit on 07-08-2017.
@@ -59,38 +55,11 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 final FirebaseUser user=firebaseAuth.getCurrentUser();
                 if(user!=null){
-                    FirebaseDatabase.getInstance().getReference().child("usertype").child(mFirebaseAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            UserType userType = dataSnapshot.getValue(UserType.class);
-                            if (userType!=null && userType.getType().equals("Patient")) {
-                                USER = "Patient";
-                                displayName = user.getDisplayName();
-                                displayEmail = user.getEmail();
-                                if(user.getPhotoUrl() != null)
-                                    imageUri = user.getPhotoUrl().toString();
-                                toastmessage("Successfylly signed in with type " + USER + " mail:"+ user.getEmail());
-                                onSignedInInitialize(user.getDisplayName());
-                                return;
-                            } else {
-                                USER = "Doctor";
-                                displayName = user.getDisplayName();
-                                displayEmail = user.getEmail();
-                                if(user.getPhotoUrl() != null)
-                                    imageUri = user.getPhotoUrl().toString();
-                                toastmessage("Successfylly signed in with type " + USER + " mail:"+ user.getEmail());
-                                onSignedInInitialize(user.getDisplayName());
-                                return;
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-
-
+                    displayEmail = user.getEmail();
+                    if(user.getPhotoUrl() != null)
+                        imageUri = user.getPhotoUrl().toString();
+                    toastmessage("Successfylly signed in  "+ "mail:"+ user.getEmail());
+                    onSignedInInitialize(user.getDisplayName());
                 }else {
                     onSignedOutCleanUp();
                 }
@@ -106,7 +75,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 USER = "Patient";
-                Intent intent = new Intent(LoginActivity.this,PatientLoginActivity.class);
+                Intent intent = new Intent(LoginActivity.this,PatientRegistration.class);
                 startActivity(intent);
             }
         });
@@ -134,9 +103,6 @@ public class LoginActivity extends AppCompatActivity {
 //                .requestIdToken(getString(R.string.default_web_client_id))
 //                .requestEmail()
 //                .build();
-
-
-
 
         googleButton.setOnClickListener(new Button.OnClickListener(){
             @Override
@@ -167,37 +133,6 @@ public class LoginActivity extends AppCompatActivity {
                                          } else {
                                              onSignedInInitialize(mFirebaseAuth.getCurrentUser().getDisplayName());
                                              //Check Type of User
-//                                             final FirebaseUser user=mFirebaseAuth.getCurrentUser();
-//                                             FirebaseDatabase.getInstance().getReference().child("usertype").child(mFirebaseAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
-//                                                 @Override
-//                                                 public void onDataChange(DataSnapshot dataSnapshot) {
-//                                                     UserType userType = dataSnapshot.getValue(UserType.class);
-//                                                     if (userType!=null && userType.getType().equals("Patient")) {
-//                                                         USER = "Patient";
-//                                                         displayName = user.getDisplayName();
-//                                                         displayEmail = user.getEmail();
-//                                                         if(user.getPhotoUrl() != null)
-//                                                             imageUri = user.getPhotoUrl().toString();
-//                                                         toastmessage("Successfylly signed in with type " + USER + " mail:"+ user.getEmail());
-//                                                         onSignedInInitialize(mFirebaseAuth.getCurrentUser().getDisplayName());
-//                                                         return;
-//                                                     } else {
-//                                                         USER = "Doctor";
-//                                                         displayName = user.getDisplayName();
-//                                                         displayEmail = user.getEmail();
-//                                                         if(user.getPhotoUrl() != null)
-//                                                             imageUri = user.getPhotoUrl().toString();
-//                                                         toastmessage("Successfylly signed in with type " + USER + " mail:"+ user.getEmail());
-//                                                         onSignedInInitialize(user.getDisplayName());
-//                                                         return;
-//                                                     }
-//                                                 }
-//
-//                                                 @Override
-//                                                 public void onCancelled(DatabaseError databaseError) {
-//
-//                                                 }
-//                                             });
                                          }
                                      }
                                  });
@@ -208,7 +143,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void signIn() {
-
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -296,7 +230,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void detachchDatabaseReadListener(){
-
     }
 
     @Override
